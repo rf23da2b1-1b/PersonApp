5c0e429ef99e7b3b6cb1ae3fa00f0706f00e004f
Peter Levinsky <pele@zealand.dk>
2024-02-09 11:04:26 +0100

    Inklusiv Test-klasse

---
+++ PersonLibTests/model/PersonTests.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PersonLib.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonLib.model.Tests
+{
+    
+
+
+
+
+    [TestClass()]
+    public class PersonTests
+    {
+        private Person person;
+
+        [TestInitialize]
+        public void BeforeEachTest()
+        {
+            person = new Person();
+        }
+
+
+        [TestMethod()]
+        [DataRow(1000)]
+        [DataRow(99999)]
+        [DataRow(5000)]
+        public void PersonIdOk(int id)
+        {
+            //Arrange
+            //Person person = new Person();
+            int expectedId = id;
+
+
+            //Act
+            person.Id = id;
+
+            // Assert
+            Assert.AreEqual(expectedId, person.Id);
+        }
+
+        [TestMethod()]
+        [DataRow(999)]
+        [DataRow(100000)]
+        [DataRow(-5000)]
+        public void PersonIdNotOK(int id)
+        {
+            //Arrange
+            //Person person = new Person();
+            int expectedId = id;
+
+
+            //Act + Assert
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => person.Id = id);
+        }
+
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
